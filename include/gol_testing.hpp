7a32dde88b635a5425823d75db678998174ef978
Miro Palmu <miro.palmu@helsinki.fi>
2023-01-11 14:04:32 +0200

    Added testing function and tests for it

--- include/gol_testing.hpp
+++ include/gol_testing.hpp
@@ -1,5 +1,48 @@
 #include <gol_concepts.hpp>
+#include <concepts>
+#include <ranges>
+#include <vector>
+#include <algorithm>
 
 namespace gol::testing {
+
+    template <typename R, typename C, typename I>
+    requires game_of_life_cell_range<R, C, I>
+    auto ranges_contains_exactly_same_cells(const R& range_1, const R& range_2) -> bool {
+        if (std::ranges::size(range_1) != std::ranges::size(range_2)) {
+            return false; // Different amount of cells
+        }
+
+        const auto cells = std::ranges::size(range_1);
+
+        // Because vector of bool is weird we use int as boolean
+        auto cells_from_range_2_found_in_range_1 = std::vector<int>(cells, 0);
+        auto cells_from_range_1_found_in_range_2 = std::vector<int>(cells, 0);
+
+        for (size_t i = 0; i < cells; ++i) {
+            for (size_t j = 0; j < cells; ++j) {
+                const auto& cell_1 = range_1[i];
+                const auto& cell_2 = range_2[j];
+
+                if (cell_1 == cell_2) {
+
+                    if (cells_from_range_1_found_in_range_2[i] || cells_from_range_2_found_in_range_1[j]) {
+                        return false; // Same cell found twice
+                    }
+
+                    cells_from_range_1_found_in_range_2[i] = 1;
+                    cells_from_range_2_found_in_range_1[j] = 1;
+                }
+
+            }
+        }
+        // To get bool from ints
+        auto identiy = [](auto x) noexcept -> bool { return x; };
+
+        return std::ranges::all_of(cells_from_range_1_found_in_range_2, identiy)
+                && std::ranges::all_of(cells_from_range_2_found_in_range_1, identiy);
+
+    }
+
     
 }
---
+++ unit_tests/test_testing_functions.cpp
@@ -0,0 +1,48 @@
+#include <catch2/catch_template_test_macros.hpp>
+#include <gol_testing.hpp>
+#include <gol_utility.hpp>
+#include <gol_concepts.hpp>
+
+#include <vector>
+
+TEMPLATE_TEST_CASE("Tests testing functions", "[testing]", int32_t, int64_t) {
+    
+
+    SECTION("Testing functions related to cell ranges") {
+        using coordinate_t = TestType;
+        using cell_t = gol::NaturalCell<TestType>;
+        using cell_range_t = std::vector<cell_t>;
+
+        gol::game_of_life_cell_range<cell_t, coordinate_t> auto cell_range = cell_range_t{ {-1, 1}, {1, 1}, {-1, -1}, {1, -1} };
+
+        SECTION("Two cell ranges are the same (even if in different order)") {
+            gol::game_of_life_cell_range<cell_t, coordinate_t> auto another_cell_range = cell_range_t{ {-1, 1}, {-1, -1}, {1, 1}, {1, -1} };
+
+            REQUIRE(gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(cell_range, another_cell_range));
+            REQUIRE(gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(another_cell_range, cell_range));
+        }
+        
+ 
+        SECTION("Two cell ranges differ by amount of cells") {
+            gol::game_of_life_cell_range<cell_t, coordinate_t> auto another_cell_range = cell_range_t{ {-1, 1}, {1, 1}, {-1, -1} };
+
+            REQUIRE(!gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(cell_range, another_cell_range));
+            REQUIRE(!gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(another_cell_range, cell_range));
+        }
+
+        SECTION("Two cell ranges differ by one cell repeating") {
+            gol::game_of_life_cell_range<cell_t, coordinate_t> auto another_cell_range = cell_range_t{ {-1, 1}, {1, 1}, {-1, -1}, {-1, -1} };
+
+            REQUIRE(!gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(another_cell_range, cell_range));
+            REQUIRE(!gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(cell_range, another_cell_range));
+        } 
+
+
+        SECTION("Two cell ranges differ by not having the same cells") {
+            gol::game_of_life_cell_range<cell_t, coordinate_t> auto another_cell_range = cell_range_t{ {-1, 2}, {1, 4}, {-1, -3}, {-2, -1} };
+
+            REQUIRE(!gol::testing::ranges_contains_exactly_same_cells<cell_range_t, cell_t, coordinate_t>(cell_range, another_cell_range));
+        }
+ 
+    }
+}
